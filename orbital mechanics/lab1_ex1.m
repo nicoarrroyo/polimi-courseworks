05ef55205f1468929d706fec633db4c9b037d063
Nicolas Arroyo <162636059+nicoarrroyo@users.noreply.github.com>
2025-10-02 10:25:36 +0200

    Update lab1_ex1.m

--- orbital mechanics/lab1_ex1.m
+++ orbital mechanics/lab1_ex1.m
@@ -7,11 +7,7 @@
 % 5. write a main script to numerically integrate the system
 %     - choose one of MATLAB's solvers
 
-% tspan = [tstart tend];
-% y0 = y(tstart);
-% [t, y] = odeXX(odefun, tspan, y0, options);
-
-
+mu_E = astroConstants(13); % [km^3 s^-2]
 
 % initial conditions
 r0 = [26578.137 0 0]; % [km]
@@ -19,5 +15,21 @@ v0 = [0 2.221 3.137]; % [km s^-1]
 y0 = [r0 v0];
 
 % time span
+a = 1/(2/norm(r0) - dot(v0,v0)/mu_E); % [km]
+T = 2*pi*sqrt(a^3/mu_E); % [s]
+tspan = linspace(0, 2*T, 1000);
 
+% set ODE solver conditions
+options = odeset("RelTol", 1e-13, "AbsTol", 1e-14);
+
+% integrate
+[T, Y] = ode113(@(t,y) ode_2bp(t,y,mu_E), tspan, y0, options);
+
+% plot
+figure()
+plot3(Y(:,1), Y(:,2), Y(:,3), "-")
+xlabel("X [km]"); ylabel("Y [km]"); zlabel("Z [km]");
+title("Two-body problem orbit");
+axis equal;
+grid on;
 
