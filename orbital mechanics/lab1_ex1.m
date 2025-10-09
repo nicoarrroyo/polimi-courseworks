ddeeef15242194824c71b8ad078647d49163b8dd
Nicolas Arroyo <162636059+nicoarrroyo@users.noreply.github.com>
2025-10-09 15:34:42 +0200

    Update lab1_ex1.m

--- orbital mechanics/lab1_ex1.m
+++ orbital mechanics/lab1_ex1.m
@@ -1,10 +1,13 @@
-%% Exercise 1 - 2-Body Problem
+clear; close all; clc;
+
+% Exercise 1 - 2-Body Problem
 % Numerically integrate a Keplerian orbit (two-body problem)
-% 1. identify states of the system and physical parameters involved
-% 2. write second-order ODE describing dynamics
-% 3. reduce problem to first-order ODE
-% 4. implement odefun function
-% 5. write a main script to numerically integrate the system
+%% %%% 1. Implement the code to propogate the orbit %%%%%%%%%%%%%%%%%%%%%%%
+% a. identify states of the system and physical parameters involved
+% b. write second-order ODE describing dynamics
+% c. reduce problem to first-order ODE
+% d. implement odefun function
+% e. write a main script to numerically integrate the system
 %     - choose one of MATLAB's solvers
 
 mu_E = astroConstants(13); % [km^3 s^-2]
@@ -25,11 +28,44 @@ options = odeset("RelTol", 1e-13, "AbsTol", 1e-14);
 % integrate
 [T, Y] = ode113(@(t,y) ode_2bp(t,y,mu_E), tspan, y0, options);
 
+% get texture of earth
+earth_img = imread("EarthTexture.jpg");
+[x, y, z] = sphere(50);
+radius = 6731;
+x = radius * x;
+y = radius * y;
+z = -radius * z;
+
 % plot
 figure()
 plot3(Y(:,1), Y(:,2), Y(:,3), "-")
+hold on
+surface(x, y, z, "FaceColor", "texturemap", ...
+    "CData", earth_img, "EdgeColor", "none")
 xlabel("X [km]"); ylabel("Y [km]"); zlabel("Z [km]");
 title("Two-body problem orbit");
 axis equal;
 grid on;
+hold off
+
+%% %%% 2. Propagate the orbit for different initial  %%%%%%%%%%%%%%%%%%%%%%
+% %%% conditions and primary attracting body %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% a. for a closed orbit, initial conditions should correspond to a 
+% negative specific energy. 
+
+
+
+%% %%% 3. Analyse the results %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% a. plot the orbit over 1 period T
+% b. plot the components, norm of h, e, over 5 periods. they should be 
+% constant in magnitude and direction. 
+% c. check that h and e remain perpendicular by plotting the error
+% d. plot specific energy over 5 periods. should be constant in time. 
+% e. plot the evolution of vr and vθ (vt) over 5 periods. 
+
+% Calculate specific energy
+specificEnergy = 0.5 * norm(v0)^2 - mu_E / norm(r0);
+
+
+
 
