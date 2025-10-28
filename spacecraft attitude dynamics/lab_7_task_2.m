b35aca1b0b0b723259308fa68c71cb8c04badeaa
Nicolas Arroyo <162636059+nicoarrroyo@users.noreply.github.com>
2025-10-28 17:54:15 +0100

    Add files via upload

--- spacecraft attitude dynamics/lab_7_task_2.m
+++ spacecraft attitude dynamics/lab_7_task_2.m
@@ -26,53 +26,72 @@ e_sun = deg2rad(23.45); % eccentricity (sun) []
 % angular velocities
 w0 = [0.45; 0.52; 0.55;];
 
+% DCM
+A_BN0 = eye(3);
+
 % SRP
 J_depl = diag([ 100 25.1 91.6 ] * 10^-2); % kg m^2
 F_e = 1358; % solar radiation intensity [W m^-2]
 c = 2.998e8; % speed of light [m s^-1]
+P = F_e / c;
 
 % physical satellite properties
-N_B_body = [ % body surface normal vector
-    [1 0 0]
+N_B = [
+    [1 0 0] % body surface normal vectors
     [0 1 0]
     [-1 0 0]
     [0 -1 0] % unknown / assumed to follow 3
     [0 0 1] % unknown / assumed to follow 6
     [0 0 -1]
+    [1 0 0] % solar panel surface normal vectors
+    [-1 0 0]
+    [1 0 0]
+    [-1 0 0] % unknown / assumed to follow 3
     ];
-rho_s_d_body = [ % body surface solar values
-    [0 5 0 1]
-    [0 5 0 1]
-    [0 5 0 1]
-    [0 5 0 1]
-    [0 5 0 1]
-    [0 5 0 1]
+rho_s = [
+    [0 5] % body surface solar values "s"
+    [0 5]
+    [0 5]
+    [0 5]
+    [0 5]
+    [0 5]
+    [0 1] % solar panel surface solar values "s"
+    [0 1]
+    [0 1]
+    [0 1]
     ];
-area_body = [6 6 6 6 4 4] * 10^-2; % body surface area [m^2]
-r_F_body = [ % distance from body surface CoM to satellite CoM
-    [10 0 0]
+rho_d = [
+    [0 1] % body surface solar values "d"
+    [0 1]
+    [0 1]
+    [0 1]
+    [0 1]
+    [0 1]
+    [0 1] % solar panel surface solar values "d"
+    [0 1]
+    [0 1]
+    [0 1]
+    ];
+area = [
+    6 % body surface areas [m^2]
+    6
+    6
+    6
+    4
+    4
+    12 % solar panel surface areas [m^2]
+    12
+    12
+    12
+    ] * 10^-2;
+r_F = [
+    [10 0 0] % distances from body surface CoM to satellite CoM
     [0 10 0]
     [-10 0 0]
     [0 -10 0] % unknown / assumed to follow 3
     [0 0 15]
     [0 0 -15]
-    ];
-
-N_B_panel = [ % solar panel surface normal vectors
-    [1 0 0]
-    [-1 0 0]
-    [1 0 0]
-    [-1 0 0] % unknown / assumed to follow 3
-    ];
-rho_s_d_panel = [ % solar panel surface solar values
-    [0 1 0 1]
-    [0 1 0 1]
-    [0 1 0 1]
-    [0 1 0 1]
-    ];
-area_panel = [12; 12; 12; 12;] * 10^-2; % solar panel surface area [m^2]
-r_F_panel = [ % distance from body surface CoM to satellite CoM
-    [0 45 0]
+    [0 45 0] % distances from body surface CoM to satellite CoM
     [0 45 0]
     [0 -45 0]
     [0 -45 0]
@@ -87,7 +106,7 @@ sim_options.StopTime = "100";
 
 %% outputs
 disp("running sim")
-simout = sim("lab_7_task_1_simulink", sim_options);
+simout = sim("lab_7_task_2_simulink", sim_options);
 disp("sim complete")
 
 % time
@@ -109,62 +128,62 @@ wydot = wdot(:, 3);
 A_BN = simout.A_BN.Data;
 
 %% Plots
-TA = simout.TA.Data;
-r = simout.r.Data;
-r_N = simout.r_N.Data;   
-r_B = simout.r_B.Data;
-S_N = simout.S_N.Data;   
-S_B = simout.S_B.Data;
-
-% True anomaly
-figure();
-plot(t, rad2deg(TA), 'LineWidth', 2);
-xlabel('t [s]');
-ylabel('$\theta$ [deg]', 'Interpreter', 'latex');
-title('True Anomaly');
-grid on;
-xlim([t(1), t(end)]);
-
-% Orbital radius
-figure();
-plot(t, r, 'LineWidth', 2);
-xlabel('t [s]');
-ylabel('r [km]');
-title('Orbital Radius');
-grid on;
-xlim([t(1), t(end)]);
-yline(a, '--r', 'Semi-major axis');
-
-% Position in inertial frame
-figure();
-subplot(3,1,1);
-plot(t, r_N(:,1), 'LineWidth', 2);
-ylabel('$r_N^x$ [km]', 'Interpreter', 'latex');
-grid on;
-title('Position in Inertial Frame [km]');
-subplot(3,1,2);
-plot(t, r_N(:,2), 'LineWidth', 2);
-ylabel('$r_N^y$ [km]', 'Interpreter', 'latex');
-grid on;
-subplot(3,1,3);
-plot(t, r_N(:,3), 'LineWidth', 2);
-ylabel('$r_N^z$ [km]', 'Interpreter', 'latex');
-xlabel('t [s]');
-grid on;
-
-% Position in body frame
-figure();
-subplot(3,1,1);
-plot(t, r_B(:,1), 'LineWidth', 2);
-ylabel('$r_B^x$ [km]', 'Interpreter', 'latex');
-grid on;
-title('Position in Body Frame [km]');
-subplot(3,1,2);
-plot(t, r_B(:,2), 'LineWidth', 2);
-ylabel('$r_B^y$ [km]', 'Interpreter', 'latex');
-grid on;
-subplot(3,1,3);
-plot(t, r_B(:,3), 'LineWidth', 2);
-ylabel('$r_B^z$ [km]', 'Interpreter', 'latex');
-xlabel('t [s]');
-grid on;
+% TA = simout.TA.Data;
+% r = simout.r.Data;
+% r_N = simout.r_N.Data;   
+% r_B = simout.r_B.Data;
+% S_N = simout.S_N.Data;   
+% S_B = simout.S_B.Data;
+% 
+% % True anomaly
+% figure();
+% plot(t, rad2deg(TA), 'LineWidth', 2);
+% xlabel('t [s]');
+% ylabel('$\theta$ [deg]', 'Interpreter', 'latex');
+% title('True Anomaly');
+% grid on;
+% xlim([t(1), t(end)]);
+% 
+% % Orbital radius
+% figure();
+% plot(t, r, 'LineWidth', 2);
+% xlabel('t [s]');
+% ylabel('r [km]');
+% title('Orbital Radius');
+% grid on;
+% xlim([t(1), t(end)]);
+% yline(a, '--r', 'Semi-major axis');
+% 
+% % Position in inertial frame
+% figure();
+% subplot(3,1,1);
+% plot(t, r_N(:,1), 'LineWidth', 2);
+% ylabel('$r_N^x$ [km]', 'Interpreter', 'latex');
+% grid on;
+% title('Position in Inertial Frame [km]');
+% subplot(3,1,2);
+% plot(t, r_N(:,2), 'LineWidth', 2);
+% ylabel('$r_N^y$ [km]', 'Interpreter', 'latex');
+% grid on;
+% subplot(3,1,3);
+% plot(t, r_N(:,3), 'LineWidth', 2);
+% ylabel('$r_N^z$ [km]', 'Interpreter', 'latex');
+% xlabel('t [s]');
+% grid on;
+% 
+% % Position in body frame
+% figure();
+% subplot(3,1,1);
+% plot(t, r_B(:,1), 'LineWidth', 2);
+% ylabel('$r_B^x$ [km]', 'Interpreter', 'latex');
+% grid on;
+% title('Position in Body Frame [km]');
+% subplot(3,1,2);
+% plot(t, r_B(:,2), 'LineWidth', 2);
+% ylabel('$r_B^y$ [km]', 'Interpreter', 'latex');
+% grid on;
+% subplot(3,1,3);
+% plot(t, r_B(:,3), 'LineWidth', 2);
+% ylabel('$r_B^z$ [km]', 'Interpreter', 'latex');
+% xlabel('t [s]');
+% grid on;
