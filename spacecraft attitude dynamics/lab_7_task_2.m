a342af28f97e69630d18f0424160aa351def473e
Nicolas Arroyo <162636059+nicoarrroyo@users.noreply.github.com>
2025-10-29 17:32:05 +0100

    Add files via upload

--- spacecraft attitude dynamics/lab_7_task_2.m
+++ spacecraft attitude dynamics/lab_7_task_2.m
@@ -1,33 +1,33 @@
 clear; close all; clc;
 
 %% initial conditions
-% constants
-mu_E = astroConstants(13);
-
 % inertia
-ix = 0.070;
-iy = 0.055;
-iz = 0.025;
-I = diag([ 100 25.1 91.6 ] * 10^-2); % kg m^2
-
-% position
-a = 7093; % SMA [km]
-e = 0.5; % eccentricity []
-TA0 = deg2rad(0); % initial true anomaly [rad]
-inc = deg2rad(98.27); % inclination [rad]
-n = sqrt(mu_E/(a^3)); % average rotational rate
-T = 2*pi * n; % period [s]
-
-% sun
-n_sun = 2*pi / (60*60*24*365); % average rotational rate (sun)
-r_sun = 1.496e8;
-e_sun = deg2rad(23.45); % eccentricity (sun) []
+ix = 100.9;
+iy = 25.1;
+iz = 91.6;
+I = diag([ ix iy iz ] * 10^-2); % kg m^2
 
 % angular velocities
 w0 = [0.45; 0.52; 0.55;];
 
 % DCM
 A_BN0 = eye(3);
+
+% keplerian parameters
+a = 7093; % SMA [km]
+%e = 0.5; % eccentricity [-]
+e = 0.00277; % eccentricity [-]
+TA0 = deg2rad(0); % initial true anomaly [rad]
+inc = deg2rad(98.27); % inclination [rad]
+
+mu = astroConstants(13); % earth gravitational parameter [km^3 kg^-1 s^-2]
+n = sqrt(mu/(a^3)); % average rotational rate
+T = 2*pi / n; % period [s]
+
+% sun
+n_sun = 2*pi / (60*60*24*365); % average rotational rate (sun)
+r_sun = 1.496e8;
+e_sun = deg2rad(23.45); % eccentricity (sun) []
 
 % SRP
 % J_depl = diag([ 100 25.1 91.6 ] * 10^-2); % kg m^2
@@ -36,7 +36,7 @@ c = 2.998e8; % speed of light [m s^-1]
 P = F_e / c;
 
 % physical satellite properties
-N_B = [ % FLIP THIS
+N_B = [
     [1 0 0] % body surface normal vectors
     [0 1 0]
     [-1 0 0]
@@ -65,16 +65,16 @@ area = [
     12
     ] * 10^-2;
 r_F = [
-    [10 0 0] % distances from body surface CoM to satellite CoM
-    [0 10 0]
-    [-10 0 0]
-    [0 -10 0] % unknown / assumed to follow 3
-    [0 0 15]
-    [0 0 -15]
-    [0 45 0] % distances from body surface CoM to satellite CoM
-    [0 45 0]
-    [0 -45 0]
-    [0 -45 0]
+    [10 0 0] * 10^-2 % distances from body surface CoM to satellite CoM
+    [0 10 0] * 10^-2
+    [-10 0 0] * 10^-2
+    [0 -10 0] * 10^-2 % unknown / assumed to follow 3
+    [0 0 15] * 10^-2 % unknown / assumed to follow 6
+    [0 0 -15] * 10^-2
+    [0 45 0] * 10^-2 % distances from body surface CoM to satellite CoM
+    [0 45 0] * 10^-2
+    [0 -45 0] * 10^-2
+    [0 -45 0] * 10^-2
     ];
 
 % simulation options
@@ -92,79 +92,7 @@ disp("sim complete")
 % time
 t = simout.tout;
 
-% angular velocity
-w = simout.w.Data;
-wx = w(:, 1);
-wy = w(:, 2);
-wz = w(:, 3);
-
-% angular acceleration
-wdot = simout.wdot.Data;
-wxdot = wdot(:, 1);
-wzdot = wdot(:, 2);
-wydot = wdot(:, 3);
-
-% DCM - body
-A_BN = simout.A_BN.Data;
-
-%% Plots
-% TA = simout.TA.Data;
-% r = simout.r.Data;
-% r_N = simout.r_N.Data;   
-% r_B = simout.r_B.Data;
-% S_N = simout.S_N.Data;   
+S_N = simout.S_N.Data;
 S_B = simout.S_B.Data;
 S_B_hat = simout.S_B_hat.Data;
-% 
-% % True anomaly
-% figure();
-% plot(t, rad2deg(TA), 'LineWidth', 2);
-% xlabel('t [s]');
-% ylabel('$\theta$ [deg]', 'Interpreter', 'latex');
-% title('True Anomaly');
-% grid on;
-% xlim([t(1), t(end)]);
-% 
-% % Orbital radius
-% figure();
-% plot(t, r, 'LineWidth', 2);
-% xlabel('t [s]');
-% ylabel('r [km]');
-% title('Orbital Radius');
-% grid on;
-% xlim([t(1), t(end)]);
-% yline(a, '--r', 'Semi-major axis');
-% 
-% % Position in inertial frame
-% figure();
-% subplot(3,1,1);
-% plot(t, r_N(:,1), 'LineWidth', 2);
-% ylabel('$r_N^x$ [km]', 'Interpreter', 'latex');
-% grid on;
-% title('Position in Inertial Frame [km]');
-% subplot(3,1,2);
-% plot(t, r_N(:,2), 'LineWidth', 2);
-% ylabel('$r_N^y$ [km]', 'Interpreter', 'latex');
-% grid on;
-% subplot(3,1,3);
-% plot(t, r_N(:,3), 'LineWidth', 2);
-% ylabel('$r_N^z$ [km]', 'Interpreter', 'latex');
-% xlabel('t [s]');
-% grid on;
-% 
-% % Position in body frame
-% figure();
-% subplot(3,1,1);
-% plot(t, r_B(:,1), 'LineWidth', 2);
-% ylabel('$r_B^x$ [km]', 'Interpreter', 'latex');
-% grid on;
-% title('Position in Body Frame [km]');
-% subplot(3,1,2);
-% plot(t, r_B(:,2), 'LineWidth', 2);
-% ylabel('$r_B^y$ [km]', 'Interpreter', 'latex');
-% grid on;
-% subplot(3,1,3);
-% plot(t, r_B(:,3), 'LineWidth', 2);
-% ylabel('$r_B^z$ [km]', 'Interpreter', 'latex');
-% xlabel('t [s]');
-% grid on;
+F_i = simout.F_i.Data;
