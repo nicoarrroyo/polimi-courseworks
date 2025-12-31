55763a5ff51e4b4c52cc636a195d922c0030a020
Nicolas Arroyo <162636059+nicoarrroyo@users.noreply.github.com>
2025-12-31 19:23:15 +0100

    Update get_asteroid_state.m

--- orbital mechanics/project/Assignment1/get_asteroid_state.m
+++ orbital mechanics/project/Assignment1/get_asteroid_state.m
@@ -10,28 +10,7 @@ function [r, v] = get_asteroid_state(mjd2000, body_id, mu)
 %   r - Position vector [km] (column vector)
 %   v - Velocity vector [km s^-1] (column vector)
 
-    [kep, ~] = uplanet(mjd2000, body_id);
-    [kep, ~, M] = ephAsteroids(mjd2000, body_id);
-    
-    % --- convert from mean to eccentric anomaly anomaly ---
-    % M = E - esinE -> solve iteratively
-    tol = 10^-8;
-    E = M; e = kep(2);
-    ratio = 1;
-    while abs(ratio) > tol
-        f = E - e * sin(E) - M;
-        f_prime = 1 - e * cos(E);
-
-        ratio = f / f_prime;
-        E = E - ratio;
-    end
-
-    % --- convert from eccentric anomaly to true anomaly ---
-    % TA = 2arctan(tan(E/2) / sqrt((1-e)/(1+e)))
-    I'M SO DUMB!! 
-    it gives you a kep vector
-    which has true anomaly! 
-    no need to mess around with converting it from mean anomaly
+    [kep, ~, ~] = ephAsteroids(mjd2000, body_id);
 
     [r, v] = kep2car(...
         kep(1), ...  % Semi-major axis
