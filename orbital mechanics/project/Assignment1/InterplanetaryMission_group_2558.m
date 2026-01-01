d6ca4475d21fa9bbc2a31eeb1395ac4e416ec907
Nicolas Arroyo <162636059+nicoarrroyo@users.noreply.github.com>
2026-01-02 00:24:14 +0100

    Refactor planet and asteroid variables in mission script

--- orbital mechanics/project/Assignment1/InterplanetaryMission_group_2558.m
+++ orbital mechanics/project/Assignment1/InterplanetaryMission_group_2558.m
@@ -25,9 +25,9 @@ planet_dep.id = 1;
 planet_dep.mu = astroConstants(10 + planet_dep.id);
 
 % --- Flyby Planet ---
-planet_fb.name = "Earth";
-planet_fb.id = 3;
-planet_fb.mu = astroConstants(10 + planet_fb.id);
+planet_ganame = "Earth";
+planet_gaid = 3;
+planet_gamu = astroConstants(10 + planet_gaid);
 
 % --- Arrival Asteroid ---
 asteroid_arr.id = 316801;
@@ -46,7 +46,7 @@ asteroid_arr.name = "N." + asteroid_arr.id;
 %% 2. Evaluate Δ𝑣tot for a grid of departure and arrival times covering ...
 % the time windows provided for the Mercury-Earth leg.
 % --- create array of departure and arrival times ---
-leg1.dep_id = planet_dep.id; leg1.arr_id = planet_fb.id;
+leg1.dep_id = planet_dep.id; leg1.arr_id = planet_gaid;
 leg1.early_dept_mjd2000 = travel_window.start_mjd2k;
 leg1.early_arr_mjd2000 = travel_window.start_mjd2k;
 leg1.late_dept_mjd2000 = travel_window.end_mjd2k;
@@ -168,7 +168,6 @@ for i = 1:steps
     [leg2.R_arr_list(i, :), leg2.V_arr_list(i, :)] = ...
         get_asteroid_state(leg2.dep_times(i), leg2.arr_id, mu_sun);
 end
-[asteroid_arr.kep1, ~, asteroid_arr.M1] = ephAsteroids(leg2.dep_times(i), leg2.arr_id);
 
 % --- conduct grid search ---
 leg2.dvtot = NaN(steps, steps);
@@ -251,35 +250,36 @@ t2 = mjd_opt_arr_grid * 24 * 3600; % arrival second
 tof = tof_days_opt_grid * 24 * 3600; % time of flight in seconds
 options = odeset("RelTol", 1e-13, "AbsTol", 1e-14);
 
-% --- get planet positions ---
-[RD1, VD1] = get_planet_state(mjd_opt_dep_grid, dep_id, mu_sun);
-[RA1, VA1] = get_planet_state(mjd_opt_dep_grid, arr_id, mu_sun);
-[RD2, VD2] = get_planet_state(mjd_opt_arr_grid, dep_id, mu_sun);
-[RA2, VA2] = get_planet_state(mjd_opt_arr_grid, arr_id, mu_sun);
+% --- get planet and asteroid positions ---
+[RD1, VD1] = get_planet_state(mjd_opt_dep_grid, planet_dep.id, mu_sun);
+[RGA1, VGA1] = get_planet_state(mjd_opt_dep_grid, planet_ga.id, mu_sun);
+[RA1, VA1] = get_asteroid_state(mjd_opt_dep_grid, asteroid_arr.id, mu_sun);
+
+[RD2, VD2] = get_planet_state(mjd_opt_arr_grid, planet_dep.id, mu_sun);
+[RGA2, VGA2] = get_planet_state(mjd_opt_arr_grid, planet_ga.id, mu_sun);
 
 [~, ~, ~, ~, V_transfer1, V_transfer2, ~, ~] = ...
-    lambertMR( RD1, RA2, tof, mu_sun, 0, 0, 0, 0 );
+    lambertMR( RD1, RGA2, tof, mu_sun, 0, 0, 0, 0 );
 
-% a. propagate the transfer arc
-y_transfer = [RD1 V_transfer1]; % transfer arc initial state vector
-tspan = linspace(t1, t2);
-
-[~, Y] = ode113(@(t,y) ode_2bp(t,y,mu_sun), tspan, y_transfer, options);
-R_transfer = Y(:, 1:3);
-V_transfer = Y(:, 4:6);
+% a. propagate the transfer arcs
+% y_transfer = [RD1 V_transfer1]; % transfer arc initial state vector
+% tspan = linspace(t1, t2);
+% 
+% [~, Y] = ode113(@(t,y) ode_2bp(t,y,mu_sun), tspan, y_transfer, options);
+% R_transfer = Y(:, 1:3);
+% V_transfer = Y(:, 4:6);
 
 % b. propagate the unused segment of the transfer arc
-y_transfer_unused = [RA2 V_transfer2]; % transfer arc initial state vector
-a_transfer = 1 / (2 / norm(RA2) - dot(V_transfer2, V_transfer2) / mu_sun); % semi major axis [km]
-T_unused = 2*pi * sqrt(a_transfer^3 / mu_sun); % orbital period [s]
-tspan_unused = linspace(t2, t2 + T_unused - t1); % integration time span array
-
-[~, Y] = ode113(@(t,y) ode_2bp(t,y,mu_sun), tspan_unused, y_transfer_unused, options);
-R_transfer_unused = Y(:, 1:3);
-V_transfer_unused = Y(:, 4:6);
+% y_transfer_unused = [RA2 V_transfer2]; % transfer arc initial state vector
+% a_transfer = 1 / (2 / norm(RA2) - dot(V_transfer2, V_transfer2) / mu_sun); % semi major axis [km]
+% T_unused = 2*pi * sqrt(a_transfer^3 / mu_sun); % orbital period [s]
+% tspan_unused = linspace(t2, t2 + T_unused - t1); % integration time span array
+% 
+% [~, Y] = ode113(@(t,y) ode_2bp(t,y,mu_sun), tspan_unused, y_transfer_unused, options);
+% R_transfer_unused = Y(:, 1:3);
+% V_transfer_unused = Y(:, 4:6);
 
 % c. propagate the departure planet orbit
-% i. during transfer
 y_D = [RD1, VD1];
 a_D = 1 / (2 / norm(RD1) - dot(VD1, VD1) / mu_sun); % semi major axis [km]
 T_D = 2*pi * sqrt(a_D^3 / mu_sun); % orbital period [s]
@@ -288,46 +288,48 @@ tspan_D = linspace(t1, t2); % integration time span array
 [~, Y] = ode113(@(t,y) ode_2bp(t,y,mu_sun), tspan_D, y_D, options);
 R_D = Y(:, 1:3);
 V_D = Y(:, 4:6);
-% ii. full orbit
-% iii. transfer window
 
 % d. propagate the arrival planet orbit
-% i. during transfer
-y_A = [RA1, VA1];
-a_A = 1 / (2 / norm(RA1) - dot(VA1, VA1) / mu_sun); % semi major axis [km]
+y_A = [RGA1, VGA1];
+a_A = 1 / (2 / norm(RGA1) - dot(VGA1, VGA1) / mu_sun); % semi major axis [km]
 T_A = 2*pi * sqrt(a_A^3 / mu_sun); % orbital period [s]
 tspan_A = linspace(t1, t2); % integration time span array
 
 [~, Y] = ode113(@(t,y) ode_2bp(t,y,mu_sun), tspan_A, y_A, options);
 R_A = Y(:, 1:3);
 V_A = Y(:, 4:6);
-% ii. full orbit
-% iii. transfer window
 
 % --- plot ---
 figure("Name", "Orbit Plot");
 
-% a. transfer arc
-plot3(R_transfer(:, 1), R_transfer(:, 2), R_transfer(:, 3), "y"); hold on;
+% a. transfer arcs
+%plot3(R_transfer1(:, 1), R_transfer1(:, 2), R_transfer1(:, 3), "y"); hold on;
+%plot3(R_transfer2(:, 1), R_transfer2(:, 2), R_transfer2(:, 3), "y");
 
-% b. unused segment of transfer arc
-%plot3(R_transfer_unused(:, 1), R_transfer_unused(:, 2), R_transfer_unused(:, 3), "--y");
+% b. unused segment of transfer arcs
+%plot3(R_transfer_unused1(:, 1), R_transfer_unused1(:, 2), R_transfer_unused1(:, 3), "--y");
+%plot3(R_transfer_unused2(:, 1), R_transfer_unused2(:, 2), R_transfer_unused2(:, 3), "--y");
 
 % c. departure planet orbit
 plot3(R_D(:, 1), R_D(:, 2), R_D(:, 3), "r"); % during transfer
 
-% d. arrival planet orbit
+% d. gravity-assist planet orbit
 plot3(R_A(:, 1), R_A(:, 2), R_A(:, 3), "g"); % during transfer
 
-% e. initial departure planet position
+% e. asteroid orbit
+%plot3(R_A(:, 1), R_A(:, 2), R_A(:, 3), "g"); % during transfer
+
+% f. departure planet boundary positions
 scatter3(R_D(1, 1), R_D(1, 2), R_D(1, 3), "filled", "MarkerFaceColor", "r");
 scatter3(R_D(end, 1), R_D(end, 2), R_D(end, 3), "MarkerFaceColor", "none", "MarkerEdgeColor", "r");
 
-% f. final arrival planet position
+% g. gravity-assist planet boundary positions
 scatter3(R_A(1, 1), R_A(1, 2), R_A(1, 3), "filled", "MarkerFaceColor", "g");
 scatter3(R_A(end, 1), R_A(end, 2), R_A(end, 3), "MarkerFaceColor", "none", "MarkerEdgeColor", "g");
 
-% g. sun position
+% h. asteroid boundary positions
+
+% i. sun position
 scatter3(0, 0, 0, "filled", "MarkerFaceColor", "y");
 
 % --- plot properties ---
