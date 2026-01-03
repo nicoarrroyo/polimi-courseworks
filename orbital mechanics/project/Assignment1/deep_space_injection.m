0aecd72f6ce1e5f263419767bceb84024d40b83a
Nicolas Arroyo <162636059+nicoarrroyo@users.noreply.github.com>
2026-01-03 18:10:45 +0100

    Refactor deep_space_injection function parameters and variables

--- orbital mechanics/project/Assignment1/deep_space_injection.m
+++ orbital mechanics/project/Assignment1/deep_space_injection.m
@@ -1,45 +1,94 @@
-function [V1_list, V2_list, dv_array, tof_array] = ...
+function [V_in_list, V_out_list, dv_array, tof_array] = ...
     deep_space_injection(R_dep_list, V_dep_list, R_arr_list, ...
-    V_arr_list, dep_times, arr_times, steps, full_lambert)
+    dep_times, arr_times, steps, full_lambert, dv_lim)
+
+% FUNCTIONNAME Brief one-line description of what the function does.
+%
+%   SYNTAX
+%   ------
+%   output = functionName(input1, input2)
+%   output = functionName(input1, input2, "OptionName", OptionValue)
+%
+%   DESCRIPTION
+%   -----------
+%   More detailed description of the function's purpose and behavior.
+%   Explain what the function does, any important notes about its operation,
+%   and any special cases or limitations.
+%
+%   INPUT ARGUMENTS
+%   ---------------
+%   input1			- (dataType) Description of input1.
+%            			Default: [default value if applicable]
+%
+%   input2   		- (dataType) Description of input2.
+%                  		Default: [default value if applicable]
+%
+%   OUTPUT ARGUMENTS
+%   ----------------
+%   output      	- (dataType) Description of output.
+%
+%   OPTIONAL PARAMETERS
+%   -------------------
+%   "OptionName" 	- (dataType) Description of the option.
+%                  		Default: [default value]
+%
+%   EXAMPLES
+%   --------
+%   Example 1: Basic usage
+%       output = functionName(input1, input2);
+%
+%   Example 2: With optional parameters
+%       output = functionName(input1, input2, "OptionName", value);
+%
+%   NOTES
+%   -----
+%	Performance: 
+% 	Numerical Stability: 
+% 	Usage: 
+% 	Dependencies: 
+%
+%   Author:  
+%   Date:    
+%   Updated: 
+%
+%   See also RELATEDFUNCTION1, RELATEDFUNCTION2
 
 mu_sun = astroConstants(4);
 
-V1_list = zeros(steps, 3);
-V2_list = zeros(steps, 3);
-dv_array = NaN(steps, steps);
-tof_array = NaN(steps, steps);
+V_in_list = NaN(steps, steps, 3);
+V_out_list = NaN(steps, steps, 3);
+dv_array = NaN(steps, steps, 3);
+tof_array = NaN(steps, steps, 3);
 
 for i = 1:steps
-    R1 = R_dep_list(i, :);
-    V1 = V_dep_list(i, :);
-    t1 = dep_times(i) * 24 * 3600;
-
-    dv_row = NaN(1, steps);
-    tof_row = NaN(1, steps);
+    temp_t1 = dep_times(i) * 24 * 3600;
+    temp_R1 = R_dep_list(i, :);
+    
     for j = 1:steps
-        t2 = arr_times(j) * 24 * 3600;
-        tof = t2 - t1;
-        
-        if tof > 0
-            R2 = R_arr_list(j, :);
-
-            [~, ~, ~, ERROR, V1_temp, V2_temp, ~, ~] = ...
-                lambertMR(R1, R2, tof, mu_sun, 0, 0, 0, 0);
-            if ERROR == 0
-                tof_row(j) = t2 - t1;
-
-                V1_list(j, :) = V1_temp;
-                V2_list(j, :) = V2_temp;
-
-                if full_lambert ~= 0 % if aiming for rendez-vous
-                    V2 = V_arr_list(j, :);
-                    dv_row(j) = norm(V1_temp - V1) + norm(V2_temp - V2);
-                else % if aiming for only gravity assist
-                    dv_row(j) = norm(V1_temp - V1);
-                end
-            end
+        % check for arrival being after departure
+        temp_t2 = arr_times(j) * 24 * 3600;
+        temp_tof = temp_t2 - temp_t1;
+        if temp_tof < 0
+            continue
         end
+
+        % check for valid lambert arc
+        temp_R2 = R_arr_list(j, :);
+        [~, ~, ~, temp_ERROR, temp_V1, temp_V2, ~, ~] = ...
+            lambertMR(temp_R1, temp_R2, temp_tof, mu_sun, full_lambert, 0, 0, 0);
+        if temp_ERROR ~= 0
+            continue
+        end
+
+        % check for reasonable delta-v
+        temp_dv = temp_V1 - V_dep_list(i, :);
+        if norm(temp_dv) > dv_lim
+            continue
+        end
+
+        V_in_list(i, j, :) = temp_V1;
+        V_out_list(i, j, :) = temp_V2;
+        dv_array(i, j, :) = temp_dv;
+        tof_array(i, j) = temp_tof;
     end
-    dv_array(i, :) = dv_row;
-    tof_array(i, :) = tof_row / (24 * 3600);
 end
