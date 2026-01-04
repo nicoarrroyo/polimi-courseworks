75a32c3a7517ea35b8d28c361b3281cc162a95de
Nicolas Arroyo <162636059+nicoarrroyo@users.noreply.github.com>
2026-01-04 12:57:16 +0100

    Implement function to find lowest delta-v mission

---
+++ orbital mechanics/project/Assignment1/find_lowest_dv_mission.m
@@ -0,0 +1,23 @@
+function find_lowest_dv_mission(dv_array, dep_times, arr_times)
+
+[min_val, min_idx_linear] = min(dv_array(:));
+[row_idx, col_idx] = ind2sub(size(dv_array), min_idx_linear);
+
+mjd_opt_dep_grid = dep_times(row_idx);
+date_d = mjd20002date(mjd_opt_dep_grid);
+
+mjd_opt_arr_grid = arr_times(col_idx);
+date_a = mjd20002date(mjd_opt_arr_grid);
+
+tof_opt_grid = mjd_opt_arr_grid - mjd_opt_dep_grid;
+
+% --- results output ---
+fprintf("=== GRID SEARCH RESULTS ===\n");
+fprintf("Min Delta V: %.4f km s^-1\n", min_val);
+fprintf("Departure:   MJD2000 %.2f\n", mjd_opt_dep_grid);
+fprintf("             Date    %.0f %.0f %.0f %.0f %.0f %.0f\n", date_d);
+fprintf("Arrival:     MJD2000 %.2f\n", mjd_opt_arr_grid);
+fprintf("             Date    %.0f %.0f %.0f %.0f %.0f %.0f\n", date_a);
+fprintf("Total ToF (days):    %.2f\n", tof_opt_grid);
+
+end
