747ae05e2a828ee5ac375b6c36c15fd5417bae83
Nicolas Arroyo <162636059+nicoarrroyo@users.noreply.github.com>
2026-05-22 09:56:13 +0200

    Update h6_tcs_sizing.m

--- space-systems-engineering-and-operations/h6_tcs_sizing.m
+++ space-systems-engineering-and-operations/h6_tcs_sizing.m
@@ -6,7 +6,7 @@ clc, clearvars, close all
 % Tmax is the minimum of the maximum temperatures
 % Tmin is the maximum of the minumum temperatures
 
-T_max_SC            = 273.15 + 40;
+T_max_SC            = 273.15 + 50;
 T_min_SC            = 273.15 - 20;
 
 %% AREA AND ALPHA/EPS CALCULATIONS (TODO)
@@ -15,42 +15,37 @@ T_min_SC            = 273.15 - 20;
 % Different absorbptivity and emissivity for the SC surface
 % Weighted average needed
 
-% Solar Arrays (front: silicon solar cells | back: conductive/painted thermal finish)
-A_sa                = 9.6;
-alpha_sa_front      = 0.90;
-eps_sa_front        = 0.85;
-alpha_sa_back       = 0.40;
-eps_sa_back         = 0.80;
+% Material properties
+alpha_mli           = 0.78;
+eps_mli             = 0.39;
 
 % MIRAS arms (front: average LICEF + coating | back: coating)
 A_arms              = 4.08;
-alpha_MIRAS_front   = (0.30*0.2 + 0.21*0.8);
-eps_MIRAS_front     = (0.30*0.2 + 0.90*0.8);
-alpha_MIRAS_back    = 0.21;
-eps_MIRAS_back      = 0.90;
+alpha_MIRAS_front   = (0.30*0.1 + eps_mli*0.9);
+eps_MIRAS_front     = (0.30*0.1 + alpha_mli*0.9);
+alpha_MIRAS_back    = eps_mli;
+eps_MIRAS_back      = alpha_mli;
 
 % PROTEUS (??? same material as MIRAS gold coating)
 A_bus               = 1;
-alpha_bus           = 0.21; 
-eps_bus             = 0.90;
+alpha_bus           = eps_mli; 
+eps_bus             = alpha_mli;
 
 % Hub
 A_hub               = 0.78;
 A_hex               = 1.098;
-alpha_hub           = 0.21;
-eps_hub             = 0.90;
+alpha_hub           = eps_mli;
+eps_hub             = alpha_mli;
 
 % TOTAL QUANTITIES
-A_tot       = A_sa*2 + A_arms*2 + A_bus*5 + A_hub*6 + A_hex;
+A_tot       = A_arms*2 + A_bus*5 + A_hub*6 + A_hex;
 r_sphere    = sqrt(A_tot/(4*pi));
 A_cross_sun = pi*r_sphere^2;
 
-alpha_SC = (A_sa*alpha_sa_front + A_sa*alpha_sa_back + ...
-            (A_arms+A_hex)*alpha_MIRAS_front + A_arms*alpha_MIRAS_back + ...
+alpha_SC = ((A_arms+A_hex)*alpha_MIRAS_front + A_arms*alpha_MIRAS_back + ...
             A_bus*5*alpha_bus + A_hub*6*alpha_hub) / A_tot;
 
-eps_SC = (A_sa*eps_sa_front + A_sa*eps_sa_back + ...
-            (A_arms+A_hex)*eps_MIRAS_front + A_arms*eps_MIRAS_back + ...
+eps_SC = ((A_arms+A_hex)*eps_MIRAS_front + A_arms*eps_MIRAS_back + ...
             A_bus*5*eps_bus + A_hub*6*eps_hub) / A_tot;
 
 fprintf('\n========== SPACECRAFT PROPERTIES ==================\n')
@@ -88,7 +83,7 @@ fprintf(' - Infrafed                = %.2f W/m^2\n', q_ir)
 Q_sun       = A_cross_sun * alpha_SC * q_sun;
 
 % ALBEDO
-K           = 0.75;                              %[TODO]
+K           = 1;                              %[TODO]
 Q_alb       = A_tot * alpha_SC * K * q_alb;
 
 % INFRARED
@@ -99,11 +94,6 @@ Q_int_mode  = [653.0-100; 761.0-190; 672.0-100; 710.2-120; 250.6-120];
 Q_int_H     = max(Q_int_mode);
 Q_int_C     = min(Q_int_mode);
 
-% EMITTED
-T_space     = 3;
-% Q_emi      = sigma * eps_SC * A_tot * (T_SC^4 - T_space^4);
-
-
 fprintf('\n========== HEAT TRANSFER RATES ====================\n')
 fprintf(' - Solar                   = %.2f W\n', Q_sun)
 fprintf(' - Albedo                  = %.2f W\n', Q_alb)
@@ -113,6 +103,10 @@ fprintf(' - Internal C              = %.2f W\n', Q_int_C)
 
 %% NON-REGULATED TEMPERATURES
 
+% EMITTED
+T_space     = 3;
+% Q_emi     = sigma * eps_SC * A_tot * (T_SC^4 - T_space^4);
+
 T_SC_hot    = ((Q_sun + Q_ir + Q_alb + Q_int_H)/(sigma*eps_SC*A_tot) + T_space^4)^0.25;
 T_SC_cold   = ((Q_ir + Q_int_C)/(sigma*eps_SC*A_tot) + T_space^4)^0.25;
 
@@ -120,34 +114,35 @@ fprintf('\n========== NON-REG TEMPERATURES ===================\n')
 fprintf(' - T_hot                   = %.1f °C\n', T_SC_hot-273.15)
 fprintf(' - T_cold                  = %.1f °C\n', T_SC_cold-273.15)
 
-
 %% RADIATORS
-marg        = 0.55;
-A_rad_max   = (A_sa + A_arms + A_bus*3 + A_hub*3)*marg;
-RAD_perc    = A_rad_max / A_tot;
+frac        = 0.5;
+A_rad_max   = (A_arms + A_bus*4 + A_hub*2)*frac;
+RAD_frac    = A_rad_max / A_tot;
 eps_rad     = 0.85;
-alpha_rad   = 0.1;
+alpha_rad   = 0.10;
 
-alpha_SC_rad = ((A_sa*alpha_sa_front + A_sa*(1-marg)*alpha_sa_back + ...
-            (A_arms+A_hex)*alpha_MIRAS_front + A_arms*(1-marg)*alpha_MIRAS_back + ...
-            A_bus*alpha_bus + A_bus*4*(1-marg)*alpha_bus + ...
-            A_hub*3*alpha_hub + A_hub*3*(1-marg)*alpha_hub ) + ...
-            A_rad_max * alpha_rad) / A_tot;
-eps_SC_rad = ((A_sa*eps_sa_front + A_sa*(1-marg)*eps_sa_back + ...
-            (A_arms+A_hex)*eps_MIRAS_front + A_arms*(1-marg)*eps_MIRAS_back + ...
-            A_bus*eps_bus + A_bus*4*(1-marg)*eps_bus + ...
-            A_hub*3*eps_hub + A_hub*3*(1-marg)*eps_hub ) + ...
-            A_rad_max * eps_rad) / A_tot;
+alpha_SC_rad = (...
+    ( ...
+    (A_arms+A_hex)*alpha_MIRAS_front + A_arms*(1-frac)*alpha_MIRAS_back + ...
+    A_bus*2*alpha_bus + A_bus*3*(1-frac)*alpha_bus + ...
+    A_hub*3*alpha_hub + A_hub*3*(1-frac)*alpha_hub ) + ...
+    A_rad_max * alpha_rad) ...
+    / A_tot;
+eps_SC_rad = ( ...
+    ( ...
+    (A_arms+A_hex)*eps_MIRAS_front + A_arms*(1-frac)*eps_MIRAS_back + ...
+    A_bus*2*eps_bus + A_bus*3*(1-frac)*eps_bus + ...
+    A_hub*3*eps_hub + A_hub*3*(1-frac)*eps_hub ) + ...
+    A_rad_max * eps_rad) ...
+    / A_tot;
 
 fprintf('\n========== with RADIATOR SC PROPERTIES ============\n')
 fprintf(' - Max Radiator Area       = %.2f m^2 \n', A_rad_max)
-fprintf(' - Radiator Fraction       = %.2f %% \n', RAD_perc*100)
-fprintf(' - Radiator Absorbtivity   = %.3f \n', alpha_rad)
+fprintf(' - Radiator Fraction       = %.2f %% \n', RAD_frac*100)
+fprintf(' - Radiator Absorptivity   = %.3f \n', alpha_rad)
 fprintf(' - Radiator Emissivity     = %.3f \n', eps_rad)
-fprintf(' - Averaged Absorbtivity   = %.3f \n', alpha_SC_rad)
+fprintf(' - Averaged Absorptivity   = %.3f \n', alpha_SC_rad)
 fprintf(' - Averaged Emissivity     = %.3f \n', eps_SC_rad)
-
-
 
 %% RADIATOR HOT TEMPERATURE SOLUTION
 
@@ -160,3 +155,33 @@ T_SC_hot_rad = ((Q_sun + Q_ir + Q_alb + Q_int_H)...
 
 fprintf('\n========== RADIATOR TEMP SOLUTION =================\n')
 fprintf(' - T_hot_rad               = %.1f °C\n', T_SC_hot_rad-273.15)
+
+%% HEATERS COLD TEMPERATURE SOLUTION
+
+Q_emit = sigma*eps_SC_rad*A_tot * ((T_min_SC + 15)^4 - T_space^4);
+Q_heat_req = (Q_emit - Q_ir - Q_int_C) * 1.25;
+
+fprintf('\n======== HEATER T_min = %.1f °C SOLUTION =========\n', T_min_SC-273.15)
+fprintf(' - Q Heaters + 25%% margin  = %.1f W\n', Q_heat_req)
+
+%% TIME-VARIANT CASE
+
+T_orb           = 100*60;
+tspan           = linspace(0, 30*T_orb, 10000);
+y0              = 273.15+10;
+
+[t, y] = ode45( ...
+    @(t, T) scThermalODE(t, T, Q_sun, Q_alb, Q_ir, Q_int_mode, eps_SC_rad), ...
+    tspan, y0);
+
+plot(t/T_orb, y-273.15)
+grid on
+title('Time-variant thermal simulation')
+xlabel('Orbits [-]'); ylabel('Temperature [°C]')
+yline(T_min_SC-273.15, color='r'); yline(T_max_SC-273.15, color='r');
+ylim([T_min_SC-50 T_max_SC+10]-273.15)
+legend('Temperature', 'Min. Temp.', 'Max. Temp.')
+
+fprintf('\n=========== TIME-VARIANT HEATER SOLUTION ==========\n')
+fprintf(' - Min. Temp. Reached      = %.1f °C\n', min(y)-273.15)
+%fprintf(' - Q Heaters + 25%% margin  = %.1f W\n', heater_power)
